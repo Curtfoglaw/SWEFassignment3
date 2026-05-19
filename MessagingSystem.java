aab03dbd5cc4a21080d63cf08adb0053dc2096ad
TedwardLeah <s4173080@student.rmit.edu.au>
2026-05-19 12:20:33 +1000

    Messaging System Skeleton code

---
+++ MessagingSystem.java
@@ -0,0 +1,28 @@
+
+public class MessagingSystem {
+
+    public boolean validateCredentials(String userName, String Password) {
+        // Check login Credentials
+        return true;
+    }
+
+    public boolean checkMessages() {
+        // Check for any messages
+        return true;
+    }
+
+    public String getMessages() {
+        // return any received messages
+        return "This is a message";
+    }
+
+    public String findUserID(String userName) {
+        // Correlate username and user ID
+        return "ij4ewioafj";
+    }
+
+    public boolean sendMessage(String message, String userID) {
+        // send a message to the user
+        return true;
+    }
+}
